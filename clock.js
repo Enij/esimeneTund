150abd1d8194ea7b76f496494fbf37e8c3f5fc4b
Markus Suurekivi <Enij@users.noreply.github.com>
2016-02-03 11:21:00 +0200

    added a digital clock

---
+++ clock.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+
+  var clock = document.getElementById('clock');
+
+  writeDate(); //Selleks, et ei oleks näha esialgset 0:0:0, instant clock
+
+  //window.setInterval(writeDate, 500);
+  window.setInterval(function(){
+    writeDate();
+  });
+};
+
+function writeDate(){
+  var today = new Date();
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+
+  clock.innerHTML = addZeroBefore(hours) + ':' + addZeroBefore(minutes) + ':' + addZeroBefore(seconds);
+
+  //setTimeout(writeDate, 1000);
+}
+
+function addZeroBefore(number){
+  if(number >= 0 && number < 10){
+    number = '0' + number;
+  }
+  return number;
+}
