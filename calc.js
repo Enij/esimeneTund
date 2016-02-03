7754bf682fbebf97292d56c14ff1c1f6b1088fce
Markus Suurekivi <Enij@users.noreply.github.com>
2016-02-03 10:40:19 +0200

    cm to inches and vice versa calculator

---
+++ calc.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+  var cm = document.getElementById('cm');
+  var inch = document.getElementById('inch');
+
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+
+  document.getElementById('to_cm').addEventListener('click', function(){
+    cm.value = inch.value*2.54;
+  });
+
+};
+
+function convertToInch(){
+  inch.value = cm.value/2.54;
+}
